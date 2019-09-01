ec13d9d631a52477e448dccf7a9d454c78366998
Антон Семенец <semenets.anton@gmail.com>
2019-09-01 22:33:27 +0300

    Initial Commit

---
+++ UIStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIStoryboard
+//
+//  Created by Антон Семенец on 01/09/2019.
+//  Copyright © 2019 Anton Semenets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
